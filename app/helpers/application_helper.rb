67505960304d972ef4f900c5f1f78191ea4bdc35
arthurdavidonisrd <davidonis007gold@gmail.com>
2025-05-13 13:56:26 -0300

    Usando helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def set_date_ptbr(us_date)
+    us_date.strftime("%d/%m/%Y")
+  end
+
+  def main_app_title
+    "Crypto Wallet"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "welcome/index"
   resources :coins
+  get "/inicio", to: "welcome#index"
   root "welcome#index"
   get "up" => "rails/health#show", as: :rails_health_check
 end
