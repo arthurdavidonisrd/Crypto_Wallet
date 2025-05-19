49932d6b69c90f5b8ee13d9e6ae0b50aefb376ae
arthurdavidonisrd <davidonis007gold@gmail.com>
2025-05-19 15:09:16 -0300

    Generating Mining Type scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_040243) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_19_180839) do
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "description"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
