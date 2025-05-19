a43b8ec9e6872e58f0f3ec982f66dc2bb77835bc
arthurdavidonisrd <davidonis007gold@gmail.com>
2025-05-18 23:31:00 -0300

    Refactoring Controller

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,41 +1,29 @@
 class CoinsController < ApplicationController
   layout "adm"
   before_action :set_coin, only: %i[ show edit update destroy ]
-
-  # GET /coins or /coins.json
   def index
     @coins = Coin.all
   end
 
-  # GET /coins/1 or /coins/1.json
   def show
   end
 
-  # GET /coins/new
   def new
     @coin = Coin.new
   end
 
-  # GET /coins/1/edit
   def edit
   end
 
-  # POST /coins or /coins.json
   def create
     @coin = Coin.new(coin_params)
-
-    respond_to do |format|
-      if @coin.save
-        format.html { redirect_to @coin, notice: "Coin was successfully created." }
-        format.json { render :show, status: :created, location: @coin }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @coin.errors, status: :unprocessable_entity }
-      end
+    if @coin.save
+      redirect_to @coin, notice: "Coin was successfully created."
+    else
+        render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /coins/1 or /coins/1.json
   def update
     respond_to do |format|
       if @coin.update(coin_params)
@@ -48,23 +36,16 @@ class CoinsController < ApplicationController
     end
   end
 
-  # DELETE /coins/1 or /coins/1.json
   def destroy
     @coin.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to coins_path, status: :see_other, notice: "Coin was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to coins_path, status: :see_other, notice: "Coin was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def coin_params
       params.expect(coin: [ :description, :acronym, :url_image ])
     end
