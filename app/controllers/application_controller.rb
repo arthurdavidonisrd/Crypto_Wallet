37d309395167122352406c00adadc91541662378
arthurdavidonisrd <davidonis007gold@gmail.com>
2025-05-21 23:14:13 -0300

    Creating links for each language app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def set_locale
+    I18n.locale = params[:locale] || cookies[:locale] || I18n.default_locale
+    cookies[:locale] = I18n.locale
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
-  def locale_lang(locale)
-    locale == :en ? "Inglês" : "Portguês do Brasil"
+  def rails_environment
+    Rails.env
   end
 
+  def locale_lang(locale)
+    case locale.to_s
+    when "pt-BR" then "Portugu\u00EAs (Brasil)"
+    when "en"    then "Ingl\u00EAs"
+    when "es"    then "Espanhol"
+    else "Desconhecido (#{locale})"
+    end
+  end
 
   def main_app_title
-    "Crypto Wallet"
-  end
-
-  def rails_environment
-    if Rails.env.development?
-     "desenvolvimento"
-    else
-     "producao"
-    end
+    "Minha Aplicação"
   end
 end
